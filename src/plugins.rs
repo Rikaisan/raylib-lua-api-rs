90dad04d5d4df6b043264e8cb091d7b9c9a0f1ee
Rikai <rikaisensei@gmail.com>
2024-07-05 23:09:27 -0500

    Implement Plugin Manager

--- src/plugins.rs
+++ src/plugins.rs
@@ -1,45 +1,62 @@
-use std::{fs::File, io::Read, path::PathBuf};
+use std::{collections::HashMap, fs::File, io::Read, path::PathBuf};
 
 use mlua::prelude::*;
 
+use crate::error::PluginError;
+
+
 #[derive(Debug, Default)]
-struct Plugin {
-    name: String,
-    state: Lua
+pub struct Plugin {
+    pub name: String,
+    pub state: Lua
 }
 
 #[derive(Debug, Default)]
-struct Manager {
-    plugins: Vec<Plugin>
+pub struct PluginManager {
+    app_name: String,
+    plugins: HashMap<String, Plugin>
 }
 
-impl Manager {
-    fn new(plugin_dir: impl Into<PathBuf>) -> anyhow::Result<Self> {
+impl PluginManager {
+    pub fn new(plugin_dir: impl Into<PathBuf>, app_name: impl Into<String>) -> Result<Self, PluginError> {
         let mut manager = Self {
+            app_name: app_name.into(),
             ..Default::default()
         };
 
-        match std::fs::read_dir(plugin_dir.into()) {
-            Ok(dir) => {
-                for entry in dir.into_iter().filter( |entry| entry.as_ref().unwrap().file_type().unwrap().is_dir() ) {
-                if let Ok(mut file) = File::open(entry.unwrap().path()) {
-                    let mut file_buffer = String::new();
-                    file.read_to_string(&mut file_buffer)?;
-                    let lua = Lua::new();
-                    lua.load(file_buffer).exec()?;
-                    let name = lua.globals().get::<_, String>("PLUGIN_NAME").unwrap_or("No Name".into());
-                    manager.plugins.push(Plugin {
-                        name,
-                        state: lua
-                    });
-                }
+        let dir = std::fs::read_dir(plugin_dir.into())?;
+        let files = dir.into_iter().filter( |entry| entry.as_ref().unwrap().file_type().unwrap().is_file() && entry.as_ref().unwrap().file_name().to_str().unwrap().ends_with(".lua") );
+        for entry in files {
+            if let Ok(mut file) = File::open(entry.as_ref().unwrap().path()) {
+                let mut file_buffer = String::new();
+                file.read_to_string(&mut file_buffer)?;
+
+                let lua = Lua::new();
+                lua.globals().set("APP_NAME", manager.app_name.as_str())?;
+                lua.load(file_buffer).exec()?;
+
+                let name = lua.globals().get::<_, String>("PLUGIN_NAME").map_err(|_|PluginError::PluginNameMissing(entry.as_ref().unwrap().file_name().into_string().unwrap()))?;
+                if manager.plugins.contains_key(&name) { return Err(PluginError::PluginExists(name)) }
+                manager.plugins.insert(name.clone(), Plugin { name, state: lua });
             }
-        },
-            Err(_) => { println!("Plugin dir doesn't exist!") },
         }
-
-        // manager.plugins.push();
-
         Ok(manager)
+    }
+
+    pub fn tick(&self) -> Result<(), PluginError> {
+        for plugin in self.plugins.values() {
+            if let Ok(update) = plugin.state.globals().get::<_, LuaFunction>("Update") {
+                update.call(())?
+            }
+        }
+        Ok(())
+    }
+
+    pub fn get_plugin(&self, name: impl Into<String>) -> Option<&Plugin> {
+        self.plugins.get(&name.into())
+    }
+
+    pub fn get_plugins(&self) -> Vec<&Plugin> {
+        self.plugins.values().collect()
     }
 }
