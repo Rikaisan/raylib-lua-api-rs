76f9c233ed821e925011d4b854237d1bfeb955b9
Rikai <rikaisensei@gmail.com>
2024-07-05 23:13:57 -0500

    update scripts

---
+++ scripts/circles.lua
@@ -0,0 +1,13 @@
+PLUGIN_NAME = "circles"
+
+CIRCLE_POS = { 10, 10 }
+
+function Update()
+    CIRCLE_POS[1] = CIRCLE_POS[1] + 1
+    CIRCLE_POS[2] = CIRCLE_POS[2] + 2
+end
+
+function Draw(screen)
+    screen:draw_circle(CIRCLE_POS[1], screen.size.y / 2 - 100, 20)
+    screen:draw_circle(CIRCLE_POS[2], screen.size.y / 2 + 100, 20)
+end
--- scripts/options.lua
+++
@@ -1,5 +0,0 @@
-TITLE = "Hello from " .. APP_NAME .. "!"
-
-function Draw(screen)
-    screen:draw_circle(screen.size.x / 2, screen.size.y / 2 - 100, 20)
-end
