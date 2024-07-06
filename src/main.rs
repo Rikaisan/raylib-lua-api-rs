a306578a581c65cd4d16d5ed114d7bd5124e9b48
Rikai <rikaisensei@gmail.com>
2024-07-05 23:13:50 -0500

    update core logic

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,14 @@
-mod shapes;
+mod plugins;
 mod surface;
+mod shapes;
+mod error;
 
-use std::{fs::File, io::Read, path::PathBuf};
-use raylib::prelude::*;
-use mlua::prelude::*;
+use error::*;
 use surface::{Surface, WindowSize};
+use plugins::PluginManager;
+use raylib::prelude::*;
 
-fn get_lua_state(file_path: impl Into<PathBuf>, app_name: &str) -> Result<Lua, LuaError> {
-    let mut file_content = String::new();
-    File::open(file_path.into())?.read_to_string(&mut file_content)?;
-
-    let lua = Lua::new();
-    lua.globals().set("APP_NAME", app_name)?;
-    lua.load(&file_content).exec()?;
-    Ok(lua)
-}
-
-fn get_title(lua: &Lua) -> Result<String, LuaError> {
-    lua.globals().get::<_, String>("TITLE")
-}
-
-fn populate_external_surface(lua: &Lua, surface: &mut Surface) -> mlua::Result<()> {
-    lua.scope(|scope| {
-        let surface = scope.create_userdata_ref_mut(surface)?;
-        lua.globals().get::<_, LuaFunction>("Draw")?.call(surface)
-    })
-}
-
-fn main() {
+fn main() -> Result<(), AppError> {
     let window_size = WindowSize(640, 480);
 
     let (mut rl, thread) = raylib::init()
@@ -35,22 +16,22 @@ fn main() {
         .title("Hello, World!")
         .build();
 
-    let test_file = get_lua_state("scripts/options.lua", "Raylib Lua API").unwrap_or(Lua::new());
+    rl.set_target_fps(60);
 
-    let title = get_title(&test_file).unwrap_or("LuaShapes".into());
-    let text_size = rl.get_font_default().measure_text(title.as_str(), 20.0, 2.0);
-
+    let plugin_manager = PluginManager::new("scripts", "Raylib Lua API").expect("Error loading scripts");
     let mut surface: Surface = Surface::new(window_size);
 
+    let title = "Hey! :)";
+    let text_size = rl.get_font_default().measure_text(title, 20.0, 2.0);
+
     while !rl.window_should_close() {
-        if let Err(e) = populate_external_surface(&test_file, &mut surface) { println!("Error populating frame: {e}") }
+        plugin_manager.tick()?;
+        if let Err(e) = surface.populate_from_plugins(plugin_manager.get_plugins()) { println!("Error populating frame: {e}") }
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::WHITE);
-        // d.draw_line(0, 240, 640, 240, Color::RED); // H
-        // d.draw_line(320, 0, 320, 480, Color::RED); // V
         d.draw_text(
-            title.as_str(),
+            title,
             (320.0 - text_size.x / 2.0) as i32,
             (240.0 - text_size.y / 2.0) as i32,
             20,
@@ -59,4 +40,6 @@ fn main() {
 
         surface.draw(&mut d);
     }
+
+    Ok(())
 }
