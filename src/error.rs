09407cd704e8f2758162308fa6afcde8e259bb0b
Rikai <rikaisensei@gmail.com>
2024-07-05 23:07:57 -0500

    Add custom errors

---
+++ src/error.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum PluginError {
+    #[error("'PLUGIN_NAME' global variable doesn't exist on '{0}'!")]
+    PluginNameMissing(String),
+    #[error("plugin with the name '{0}' already exists!")]
+    PluginExists(String),
+    #[error("error loading plugins")]
+    IOError(#[from] io::Error),
+    #[error("error executing lua file")]
+    LuaError(#[from] mlua::Error)
+}
+
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("a plugin error ocurred")]
+    PluginError(#[from] PluginError)
+}
